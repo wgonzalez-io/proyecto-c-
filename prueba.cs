6dc9d6f4d38a64465cbb42593e5ec5a95426be60
wgonzalez-io <waskartgonzalez@gmail.com>
2026-08-27 09:39:36 -0600

    agreuge mi nombre

--- prueba.cs
+++ prueba.cs
@@ -2,11 +2,10 @@ using System;
 class Programa
 {
     static void Main(){
-        Console.WriteLine("Hola mundo en C#");
+        Console.WriteLine("Hola mundo en C#, soy " + nombre);
         int edad;
         string nombre = "waskart";
         string dato;
-        bool mayor = true;
         Console.WriteLine("Escribe tu nombre: ");
         dato=Console.ReadLine();
         Console.WriteLine("Escribe tu edad:");

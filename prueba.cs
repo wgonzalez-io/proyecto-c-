1a075ed0126b70360be588681d98bb2cd8cf4e93
wgonzalez-io <waskartgonzalez@gmail.com>
2026-08-27 09:08:55 -0600

    Prueba del lenguaje

---
+++ prueba.cs
@@ -0,0 +1,15 @@
+using System;
+class Programa
+{
+    static void Main(){
+        Console.WriteLine("Hola mundo en C#");
+        int edad;
+        string nombre = "waskart";
+        string dato;
+        bool mayor = true;
+        Console.WriteLine("Escribe tu nombre: ");
+        dato=Console.ReadLine();
+
+        Console.WriteLine("Hola " + dato); 
+    }
+}
